61583f7870425416b703cfff4605835b7661beba
Alex Gate <alexkabui16@gmail.com>
2025-09-21 13:13:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  const links = document.querySelectorAll('a[href^="#"]');
-  links.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-});
+console.log('Blends by You website loaded');
