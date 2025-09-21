263e10f63eeded552ba9e1b91d5e0f0548daefb8
Alex Gate <alexkabui16@gmail.com>
2025-09-21 11:28:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// script.js - small enhancements
+
 document.querySelectorAll('a[href^="#"]').forEach(a=>{
   a.addEventListener('click', function(e){
     const target = document.querySelector(this.getAttribute('href'));
-    if(target){
-      e.preventDefault();
-      target.scrollIntoView({behavior:'smooth', block:'start'});
-    }
+    if(target){ e.preventDefault(); target.scrollIntoView({behavior:'smooth', block:'start'}); }
   });
 });
